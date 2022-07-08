2d96411447aa0ffdc400f940539041b5dac42bf1
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-07-08 08:02:22 +0200

    Update qa.py

--- qa.py
+++ qa.py
@@ -6,6 +6,24 @@ def load_qa_model():
     model = pipeline("question-answering")
     return model
 
+with st.expander("Our Team & Mission"):
+  st.write("Hello! Our team consists of Frenki Pushaj, Benjamin Rattanpal & Patrik Spindler.")
+  st.write("Our mission consists of creating an app that answers all your questions regarding celebrities.")
+  st.write("We strive to achieve this by utilizing Machine Learning :)")
+  
+with st.expander("Which Dataset do we use?"):
+  st.write("We use the Squad QA dataset provided by rajpurkar on huggingface.co . It`s regarded as one of the most popular Datasets for Question Answering.")
+ 
+st.write("Examples from our Dataset")
+
+with st.expander("In what city and state did Beyonce grow up?"):
+  st.write("Houston, Texas")
+  st.write("ID: 56bf6b0f3aeaaa14008c9601")
+
+with st.expander("When did Beyonce release Dangerously in Love?"):
+  st.write("2003")
+  st.write("ID: 56d43c5f2ccc5a1400d830ac")
+    
 qa = load_qa_model()
 st.title("Ask Questions about your Text")
 sentence = st.text_area('Please paste your text :', height=30)
